0415aca6c37c3d4fbd443afcd7964e30b082c6a9
kerolzeeq <kerolzeeq@gmail.com>
2021-07-01 01:40:12 +0800

    Added flashing animations

---
+++ 8-Bit Penalty/Assets/blink.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class blink : MonoBehaviour
+{
+    public Image bg;
+    public Text taptoplay;
+
+    public Color gray => Color.gray;
+    public Color green => Color.green;
+    public Color white => Color.white;
+
+    public void Update()
+    {
+        bg.color = Lerp(gray,white,1.0f);
+        taptoplay.color = Lerp(white,gray,4.0f);
+    }
+    
+    public Color Lerp(Color first, Color second, float speed)
+    {
+        return Color.Lerp(first, second, Mathf.Sin(Time.time * speed));
+    }
+
+    
+
+    
+}
