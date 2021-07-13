bf7b2bc2d2186bbece2b10ca7c63c038f42ef993
kerolzeeq <kerolzeeq@gmail.com>
2021-07-14 01:19:30 +0800

    Volume Slider done.

---
+++ 8-Bit Penalty/Assets/Background/SoundManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+
+    [SerializeField] Slider volumeSlider;
+    
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("MusicVolume"))
+        {
+            PlayerPrefs.SetFloat("MusicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+
+    }
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("MusicVolume", volumeSlider.value);
+    }
+}
